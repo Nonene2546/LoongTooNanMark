e39bc4814d67ce7f43f5cb36dcd6e2ff82530d85
Nonene2546 <88503339+Nonene2546@users.noreply.github.com>
2021-08-09 11:30:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const coupDay = "22 May 2014 16:30"
-const eoy = "31 Dec 2014 24:00"
 
 dayinMonth = [31,28,31,30,31,30,31,31,30,31,30,31]
 
 function renderTime(){
     let currentTime = new Date()
+    console.log(currentTime)
     timeCount =  currentTime - new Date(coupDay)
     timeCount = Math.floor(timeCount/1000)
     // console.info("second", timeCount % 60)
@@ -14,7 +14,6 @@ function renderTime(){
     // console.info("hour", Math.floor(timeCount/3600)%60)
     $('.Hour-text').text(Math.floor(timeCount/3600)%60)
     // console.info("day", Math.floor(timeCount/3600/24)%24)
-    $('.Day-text').text(Math.floor(timeCount/3600/24)%24)
     timeCount -= 19294200
     timeCount = Math.floor(timeCount/3600/24)
     let currentMonth = 0
@@ -30,12 +29,14 @@ function renderTime(){
         if(currentMonth == 1 && currentYear % 4 == 0){
             --timeCount
         }
+        ++currentMonth
     }
+    $('.Day-text').text(timeCount + dayinMonth[currentMonth+1])
     let year = Math.floor(month/12)
-    month = month % 12
+    month = month % 13
     console.info("month", month)
     console.info("year", year)
-    $('.Year-text').text(year)
+    $('.Year-text').text(year+1)
     $('.Month-text').text(month)
 }
 
